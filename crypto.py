b9156a7b648a952eac304aa27a09e3cb7e43fe8f
BlackFish <71934938+Blacksharkfin@users.noreply.github.com>
2021-06-30 19:26:42 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -15,7 +15,7 @@ st.write('''
 #getting the html
 
 crypto = st.selectbox('Which Crpyto-Currency would you like to be predicted?', ('BTC', 'ETH', 'DOGE', 'USDT', 'BNB', 'ADA', 'XRP', 'USDC', 'DOT1', 'HEX', 'UNI3', 'BCH', 'LTC', 'SOL1',
-                                                                                'LINK'))
+                                                                                'LINK', 'MATIC', 'ETC', 'THETA', 'ICP1', 'XLM', 'VET', 'FIL', 'TRX', 'XMR', 'EOS'))
 url = "https://finance.yahoo.com/quote/" + crypto + "-USD/history?p=" + crypto + "-USD"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
